d43dba10670c63a3b0a89f4a476fed5904797cb6
pabloin1 <pablocesaraltuzar04@gmail.com>
2024-04-13 21:01:06 -0600

    agregar la funcionalidad de login

--- src/user/infrastructure/UserRouter.ts
+++ src/user/infrastructure/UserRouter.ts
@@ -2,20 +2,24 @@ import express from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { GetAllUserController } from "./controllers/GetAllUserController";
 import { GetByIdUserController } from "./controllers/GetByIdUserController";
-import { createUserUseCase, getAllUseCase, getByIdUserUseCase, deleteUserUseCase } from "./dependencies";
+import {
+  createUserUseCase,
+  getAllUseCase,
+  getByIdUserUseCase,
+  deleteUserUseCase,
+  loginUserController,
+} from "./dependencies";
 import { DeleteUserController } from "./controllers/DeleteUserController";
 import { EncryptionService } from "../application/services/cifradoUseCase";
 
 const encryptionService = new EncryptionService();
-
-
 
 export const userRouter = express.Router();
 
 const createUserController = new CreateUserController(createUserUseCase);
 const getAllUserController = new GetAllUserController(getAllUseCase);
 const getByIdUserController = new GetByIdUserController(getByIdUserUseCase);
-const deleteUserController = new DeleteUserController(deleteUserUseCase)
+const deleteUserController = new DeleteUserController(deleteUserUseCase);
 
 userRouter.get("/", async (req, res) => {
   await getAllUserController.run(req, res);
@@ -32,3 +36,8 @@ userRouter.post("/", async (req, res) => {
 userRouter.delete("/:id", async (req, res) => {
   await deleteUserController.run(req, res);
 });
+
+userRouter.post(
+  "/login",
+  async (req, res) => await loginUserController.run(req, res)
+);
