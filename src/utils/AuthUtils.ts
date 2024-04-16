5c3c8881af255e96a97b52ffa84d3447fc554c0a
pabloin1 <pablocesaraltuzar04@gmail.com>
2024-04-16 14:00:07 -0600

    quitar encriptado de id

--- src/utils/AuthUtils.ts
+++ src/utils/AuthUtils.ts
@@ -3,11 +3,9 @@ import bcrypt from "bcrypt";
 
 export class AuthUtils {
   static generateAccessToken(userId: string): string {
-    // Encriptar el ID usando bcrypt
-    const encryptedUserId = bcrypt.hashSync(userId, 10);
 
-    // Firmar el token JWT con el ID encriptado como payload
-    return jwt.sign({ userId: encryptedUserId }, "your_secret_key", { expiresIn: "365d" });
+    // Firmar el token JWT con
+    return jwt.sign({ userId}, "your_secret_key", { expiresIn: "365d" });
   }
 
   static comparePasswords(plainPassword: string, hashedPassword: string): Promise<boolean> {
